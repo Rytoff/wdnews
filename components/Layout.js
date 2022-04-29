529d7bff4a63363455886ccdacb07b056a084d65
Rytoff <roman.rytoff@gmail.com>
2022-04-29 20:59:14 +0300

    layout, header & nav components created
    layout, header & nav components created

---
+++ components/Layout.js
@@ -0,0 +1,16 @@
+import { Nav } from './Nav'
+import { Header } from './Header'
+import styles from '../styles/Layout.module.css'
+
+export const Layout = ({ children }) => {
+  return (
+    <>
+      <Nav />
+      <div className={styles.container}>
+        <main className={styles.main}>
+          <Header />
+          {children}</main>
+      </div>
+    </>
+  )
+}
