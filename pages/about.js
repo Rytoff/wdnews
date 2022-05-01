ceec56765e46e906e8f7e74cc740ee2f303a3a70
Rytoff <roman.rytoff@gmail.com>
2022-05-01 16:00:09 +0300

    project finished
    project finished

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,9 @@
+import Meta from '../components/Meta'
+
 export default function About() {
   return (
     <>
+      <Meta title='About' />
       <h1>About page</h1>
     </>
   )
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,34 @@
-import Head from 'next/head'
+import Meta from '../components/Meta'
+import { server } from '../config'
+import { ArticleList } from '../components/ArticleList'
 
-export default function Home() {
+export default function Home({ articles }) {
   return (
     <>
-      <h1>Welcome to Next.js</h1>
+      <Meta title='Home' />
+      <ArticleList articles={articles} />
     </>
   )
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch(`${server}/api/articles`)
+  const articles = await res.json()
+
+  return {
+    props: {
+      articles,
+    },
+  }
+}
+
+// export const getStaticProps = async () => {
+//   const res = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=8')
+//   const articles = await res.json()
+
+//   return {
+//     props: {
+//       articles,
+//     },
+//   }
+// }
